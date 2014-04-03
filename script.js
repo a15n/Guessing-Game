bdc38c64176c876f509e5d6d6f5a3d2dc2ef522b
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-04-03 14:37:19 -0700

    HTML, CSS, JS, and jquery all functioning correctly. All requirements met.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var numberOfGuesses = 6, usedGuesses = 0, computerNumber = Math.ceil(Math.random()*100);
+var numberOfGuesses = 6, computerNumber = Math.ceil(Math.random()*100), hintHigh, hintLow;
 
 function userNum () {
     var number = $('input[name=userGuess]').val();
@@ -9,7 +9,6 @@ function userNum () {
         return false;
     }
 }
-
 function hotOrCold (userNum, computerNum) {
     var distance = Math.abs(userNum - computerNum);
     if (distance >= 25){
@@ -24,28 +23,66 @@ function hotOrCold (userNum, computerNum) {
         return (" and you are COOKED!");
     }
 }
-
 function game (userNum, computerNum) {
     if (userNum === false) {
-        return false
+        return false;
     } else if (userNum > computerNum) {
         return (userNum + " is too high");
     } else if (userNum < computerNum) {
         return (userNum + " is too low");
     } else {
-        return (userNum + " is the right number!");
+        return (userNum + " is the right number");
+    }
+}
+function hint () {
+    var plusMinus = Math.round(Math.random()); // 0 = minus, 1 = plus
+    var distanceFromComputerNumber = Math.ceil(Math.random()*10);
+    var rangeDistance = 10;
+    if (computerNumber >= 90) {
+        hintLow = 80;
+        hintHigh = 100;
+    } else if (computerNumber <= 10) {
+        hintLow = 0;
+        hintHigh = 20;
+    } else {
+        switch (plusMinus) {
+            case (0):
+                hintLow = computerNumber - distanceFromComputerNumber - rangeDistance;
+                hintHigh = computerNumber - distanceFromComputerNumber + rangeDistance;
+                break;
+            case (1):
+                hintLow = computerNumber + distanceFromComputerNumber - rangeDistance;
+                hintHigh = computerNumber + distanceFromComputerNumber + rangeDistance;
+                break;
+        }
     }
 }
 
-
 $(document).ready(function(){
-    $(document).on('click', 'button', function() {
-        var result = game(userNum(),computerNumber);
-        var hotness = hotOrCold(userNum(),computerNumber);
-        console.log(hotness);
-        $('.feedback').append('<p>' + result + hotness + '</p>'); //add in hotOrCold indicator
+    $(document).on('click', '#submit', function() {
+        var userNumber = userNum()
+        var result = game(userNumber,computerNumber);
+        var hotness = hotOrCold(userNumber,computerNumber);
+        if (userNumber !== false && numberOfGuesses !== 0) {
+            numberOfGuesses -= 1;
+            if (userNumber == computerNumber) {
+                $('.feedback').append('<p>' + result + hotness + '</p>');
+            }  else {
+                $('.feedback').append('<p>' + result + hotness + ' ' + numberOfGuesses + ' guesses remaining!</p>');
+            }
+        } else if (numberOfGuesses === 0) {
+            alert("Game over!")
+        }
     });
+    $(document).on('click', '#hint', function(){
+        numberOfGuesses -= 2;
+        hint();
+        $('.feedback').append('<p>HINT: The number is between ' + hintLow + " and " + hintHigh + ". Only " + numberOfGuesses + ' guesses remaining!</p>');
+        $('#hint').replaceWith("<button type='button' id='hint' disabled>Hint</button>")
+    })
+    $(document).on('click', '#restart', function(){
+        location.reload();
+    })
 })
 
-//Add in a description about what hotOrCold means in layman's terms
 //remove the hot or cold indicator and add in an automatically updating visual assistant
